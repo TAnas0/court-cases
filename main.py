de32a5e01d02087f1ddddd9c2f40b04a38a8e954
Anas <anastiour@gmail.com>
2022-08-02 19:33:37 +0100

    fix: Accept Terms & Conditions

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import requests
 
 
+session = requests.Session()
+
+# Agree to the terms and conditions
+x = session.get(
+    url="https://eapps.courts.state.va.us/ocis-rest/api/public/termsAndCondAccepted",
+    headers={
+        "Content-Type": "application/json;charset=UTF-8",
+    },
+)
+
 url = "https://eapps.courts.state.va.us/ocis-rest/api/public/search"
-headers = {}
+headers = {
+    "Accept": "application/json, text/plain, */*",
+}
 data = {
     "courtLevels": [],
     "divisions": [
@@ -14,6 +26,6 @@ data = {
     ],
     "searchBy": "HD"
 }
-res = requests.post(url, headers=headers, json=data)
+res = session.post(url, headers=headers, json=data)
 print(res)
 
