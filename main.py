9966d39e1cecd29dd3463f2cde14595d38e2d73d
Anas <anastiour@gmail.com>
2022-08-03 19:05:54 +0100

    WIP(main): save results for a single date into CSV

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import requests
+import pandas as pd
 from utils import accept_terms_and_conditions
-from search import get_search_page_by_hearing_date
+from search import get_search_page_by_hearing_date, search_by_hearing_date
 
 # Agree to the terms and conditions
 session = accept_terms_and_conditions()
 
-results, last_page = get_search_page_by_hearing_date(session, "07/01/2022", 9999)
+date = "07/01/2022"
+results, last_page, last_index = get_search_page_by_hearing_date(session, date, 0)
 
+results = search_by_hearing_date(session, date)
 print(results)
+df = pd.DataFrame(results)
+df.to_csv(f"{date.replace('/', '-')}.csv")
