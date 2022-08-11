944cc6b453c697f9ee2f0e6c20085b3898da7957
Anas <anastiour@gmail.com>
2022-08-11 01:40:36 +0100

    feat(main): gradually save data into CSVs per day

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import time
 from datetime import date
 import logging
@@ -25,8 +26,8 @@ session = accept_terms_and_conditions()
 # date = "08/06/2022"
 # date = "01/02/2020"
 
-start_date = date(2020, 8, 6)
-end_date = date(2020, 8, 9)
+start_date = date(2020, 1, 7)
+end_date = date(2020, 2, 1)
 
 logger.info(f"Scraping start from {start_date} to {end_date}")
 for d in date_range(start_date, end_date): # ! Last day not included
@@ -38,22 +39,45 @@ for d in date_range(start_date, end_date): # ! Last day not included
 
     details = []
     start_time = time.time()
-    print(f"Getting case details for date {d}")
+    logger.info(f"Getting case details for date {d}")
     for case in results:
         try:
+            # Handle terms not accepted errors
             case_details = get_case_details(session, case["qualifiedFips"], case["courtLevel"], case["divisionType"], case["caseNumber"])
             case_formatted = format_case_details(case | case_details)  # Merging search results with details response
             details.append(case_formatted)
+            if (details and len(details) % 100 == 0) or case == results[-1]:
+                outdir = f"output/{d.strftime('%Y')}/{d.strftime('%m')}"
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+
+                path = f"output/{d.strftime('%Y')}/{d.strftime('%m')}/{d.strftime('%d')}.csv"
+                df = pd.DataFrame(details)
+                df.to_csv(
+                    f"{path}",
+                    index=False,
+                    header=False,
+                    mode="a",
+                    columns=details[0].keys(),
+                )
+                details = []
+            # TODO Append to the day's CSV
             # raise KeyError
         except Exception as e:
             logger.exception(e)
     logger.info(f"Getting details of {len(results)} court cases took {(time.time() - start_time)/60} minutes")
 
-    df = pd.DataFrame(details)
-    df.to_csv(
-        f"output/{d.strftime('%m/%d/%Y').replace('/', '-')}-details.csv",
-        index=False,
-        columns=details[0].keys()
-    )
+    # if details:
+    #     outdir = f"output/{d.strftime('%Y')}/{d.strftime('%m')}"
+    #     if not os.path.exists(outdir):
+    #         os.makedirs(outdir, exist_ok=True)
+
+    #     path = f"output/{d.strftime('%Y')}/{d.strftime('%m')}/{d.strftime('%d')}.csv"
+    #     df = pd.DataFrame(details)
+    #     df.to_csv(
+    #         f"{path}",
+    #         index=False,
+    #         columns=details[0].keys(),
+    #     )
 
 print()
