9391073d5b0816fa7f2330d6454773a2d398e069
TAnas0 <anastiour@gmail.com>
2024-11-13 04:06:52 +0100

    feat(alembic): limit migrations to Court tables

--- src/database/alembic/env.py
+++ src/database/alembic/env.py
@@ -25,6 +25,12 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+# Limit Models included in migrations
+def include_object(object, name, type_, reflected, compare_to):
+    # Only include the "courts" table for migration
+    if type_ == "table" and name in ["courts"]:
+        return True
+    return False
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -44,6 +50,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_object=include_object,
     )
 
     with context.begin_transaction():
@@ -65,7 +72,9 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            include_object=include_object,
         )
 
         with context.begin_transaction():
