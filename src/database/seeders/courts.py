1bdce64115658af7362a75c6c208fb0c7565510d
TAnas0 <anastiour@gmail.com>
2025-03-05 21:21:27 +0000

    fix(DB): set fipsCode type to string in database seeder

--- src/database/seeders/courts.py
+++ src/database/seeders/courts.py
@@ -10,7 +10,7 @@ def seed_courts():
     COURTS_JSON_PATH = os.path.join(os.path.dirname(__file__), 'courts.json')
     COURTS_URLS_PATH = os.path.join(os.path.dirname(__file__), 'courts_urls.csv')
 
-    courts_df = pd.read_json(COURTS_JSON_PATH)
+    courts_df = pd.read_json(COURTS_JSON_PATH, dtype={"fipsCode": str})
 
     courts_urls_df = pd.read_csv(COURTS_URLS_PATH)
     courts_df = pd.merge(courts_df, courts_urls_df, on='fipsCode4', how='outer')
