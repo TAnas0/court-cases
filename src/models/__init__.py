31cee4695d4c55aecf2f79f32be2e5dc974c9f27
TAnas0 <anastiour@gmail.com>
2025-03-05 21:52:29 +0000

    fix(DB): add __init__.py for the models folder

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,12 @@
-from .case import Case, Hearing, Charge
+from .case import Case, Hearing
 from .court import Court
 from .judge import Judge
 from .participant import Participant
+from .charge import Charge, CaseCharge
 
-__all__ = ["Case", "Court", "Judge", "Participant"]
+__all__ = [
+    "Case",
+    "Court",
+    "Judge",
+    "Participant"
+]
