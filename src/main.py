219729ce57925a3264575d397b3f6705c2357672
TAnas0 <anastiour@gmail.com>
2025-04-07 18:39:26 +0100

    refactor: remove format_case_details and prepare_csv_file_location

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 from utils import (
     accept_terms_and_conditions,
-    format_case_details,
     date_range,
     get_csv_path,
     get_sample_court_case,
@@ -51,7 +50,7 @@ def scrape_day_court_cases(date):
                 case["divisionType"],
                 case["caseNumber"],
             )
-            case_formatted = format_case_details(case | case_details)  # Merging search results with details response
+            case_formatted = case | case_details  # Merging search results with details response
             details.append(case_formatted)
             if (details and len(details) % 100 == 0) or case == results[-1]:
                 df = pd.DataFrame(details)
@@ -70,31 +69,11 @@ def scrape_day_court_cases(date):
     logger.info(f"Getting details of {len(results)} court cases took {(time.time() - start_time)/60} minutes")
     
 
-def prepare_csv_file_location(date): # TODO rename
-    """
-    Ensure directory of the CSV for the date exists
-    Ensure the CSV exists with the right header
-
-    Args:
-        date (_type_): _description_
-    """
-    csv_path = get_csv_path(date)
-    csv_directory = csv_path[:-6]
-    if not os.path.exists(csv_directory):
-        os.makedirs(csv_directory, exist_ok=True)
-    try:
-        previous_results = pd.read_csv(csv_path)
-        # TODO handle cache of a day
-    except FileNotFoundError:
-        logger.info(f"No scraped data found for date {date}. Creating CSV file with header...")
-        with open(csv_path, "w") as f:
-            sample_court_case = get_sample_court_case(session)
-            f.write(f"{','.join(list(sample_court_case.keys()))}\n")
-            return
+start_date = date(2021, 1, 1)
+end_date = date(2021, 2, 1)
 
 logger.info(f"Scraping start from {start_date} to {end_date}")
 
 if __name__ == "__main__":
     for d in date_range(start_date, end_date): # ! Last day not included
-        prepare_csv_file_location(d)
         scrape_day_court_cases(d)
