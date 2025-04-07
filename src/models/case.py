9f22aa71579f41621e035322a25d858158fce710
TAnas0 <anastiour@gmail.com>
2025-04-07 16:35:14 +0100

    feat(models): set composite unique constraint on Case

--- src/models/case.py
+++ src/models/case.py
@@ -2,6 +2,7 @@
 from sqlalchemy import Column, String, Date, Boolean, ForeignKey, Integer, DateTime, Table
 from sqlalchemy.orm import relationship
 from .base import Base
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
@@ -20,26 +21,24 @@ class Case(Base):
     # TODO add a Charge attribute (and model)
     # TODO track history of a case, e.g. transfer to a new court
     __tablename__ = 'cases'
+    __table_args__ = (
+        UniqueConstraint('case_number', 'code_section', 'is_appeal', 'commenced_by', name='uq_case_key'),
+    )
 
     id = mapped_column(Integer, primary_key=True)
     case_number = Column(String, nullable=False)
-    formatted_case_number = Column(String, unique=True, nullable=False)
+    formatted_case_number = Column(String, nullable=False)
     charge_amended = Column(Boolean, default=False)
     code_section = Column(String)
     case_type = Column(String)
-
     offense_date = Column(Date)
     arrest_date = Column(Date)
-
     is_criminal = Column(Boolean, default=False)
     category = Column(String)
     sub_category = Column(String)
-
     is_appeal = Column(Boolean, default=False)
     appeal_date = Column(Date)
-
     is_active = Column(Boolean, default=False)
-
     commenced_by = Column(String)
     
     # Relationships
